d1bf3f6b92fc83f8a31f7c66022e091050140f1a
Rustymattok <you@example.com>
2019-12-22 13:19:42 +0300

    change logic

--- image_resize.py
+++ image_resize.py
@@ -5,42 +5,58 @@ from PIL import Image
 
 def get_image(path_to_original):
     if not os.path.exists(path_to_original):
-        print('not correct path to file')
         return None
     return Image.open(path_to_original)
 
 
-def get_size(size_image, width_resize, height_resize, scale_resize):
-    image_width, image_height = size_image
-    if width_resize is not None and height_resize is None:
-        size_image = (
-            int(width_resize),
-            int(round(float(width_resize) * float(image_height / image_width)))
+def get_resized_image(image, width_resized, height_resized, scale_resized):
+    if image is None:
+        return None
+    image_width, image_height = image.size
+    resized_image = None
+    if width_resized is not None \
+            and int(width_resized) > 0 \
+            and height_resized is None:
+        resized_image = (
+            int(width_resized),
+            int(round(
+                float(width_resized) * float(image_height / image_width)
+            ))
         )
-    if height_resize is not None and width_resize is None:
-        size_image = (
-            int(round(float(height_resize)*float(image_width/image_height))),
-            int(height_resize)
+    if height_resized is not None \
+            and int(height_resized) > 0 \
+            and width_resized is None:
+        resized_image = (
+            int(round(
+                float(height_resized) * float(image_width / image_height)
+            )),
+            int(height_resized)
         )
-    if scale_resize is not None:
-        size_image = (
-            int(image_width) * int(scale_resize),
-            int(image_height) * int(scale_resize)
+    if scale_resized is not None \
+            and int(scale_resized) > 0 \
+            and width_resized is None \
+            and height_resized is None:
+        resized_image = (
+            int(image_width) * int(scale_resized),
+            int(image_height) * int(scale_resized)
         )
-    return size_image
+    if resized_image is None:
+        return None
+    return image.resize(resized_image)
 
 
 def save_image(image, path_to_result):
-    if path_to_result.find(".jpg") > 0 or path_to_result.find(".png") > 0:
+    if not os.path.exists(path_to_result):
         image.save(path_to_result)
     else:
-        print('not correct out_path')
+        return None
 
 
-def get_file_name_out(image):
+def get_file_name_output(image):
     width_image, height_image = image.size
-    file_out = 'pic__' + str(width_image) + 'x' + str(height_image) + '.jpg'
-    return file_out
+    path_file_out = 'pic_{width}x{height}.jpg'.\
+        format(width=str(width_image), height=str(height_image))
+    return path_file_out
 
 
 def create_parser():
@@ -86,15 +102,23 @@ def main():
     width = args.width
     height = args.height
     scale = args.scale
-    image_input = get_image(file_path)
-    if image_input is not None:
-        image_size = image_input.size
-        resize_parameter = get_size(image_size, width, height, scale)
-        print(resize_parameter)
-        resize_image = image_input.resize(resize_parameter)
-        if file_outpath is None:
-            file_outpath = get_file_name_out(resize_image)
-        save_image(resize_image, file_outpath)
+    image = get_image(file_path)
+    if file_outpath is not None and not os.path.exists(file_outpath):
+        print("not correct directory")
+        return
+    if image is None:
+        print('not correct file path')
+        return
+    image_resized = get_resized_image(image, width, height, scale)
+    if image_resized is None:
+        print('not correct command')
+        return
+    if file_outpath is None:
+        file_outpath = get_file_name_output(image_resized)
+    else:
+        file_outpath = file_outpath + get_file_name_output(image_resized)
+    if save_image(image_resized, file_outpath) is None:
+        print('you try to rewrite current file')
 
 
 if __name__ == '__main__':
